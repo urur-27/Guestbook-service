dcaf45552f276416d52ea774a7413524ee0ed588
김민식 <u1r2r3@naver.com>
2025-05-15 17:40:26 +0900

    first commit

---
+++ src/main/java/com/sb02/guestbookservice/GuestbookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sb02.guestbookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestbookServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GuestbookServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sb02/guestbookservice/GuestbookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb02.guestbookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestbookServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
