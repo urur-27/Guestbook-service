4bbe8d8a3b8a364c86100c4d4a015dccbd4ceead
김민식 <u1r2r3@naver.com>
2025-05-19 10:26:28 +0900

    test제거

--- src/test/java/com/sb02/guestbookservice/GuestbookServiceApplicationTests.java
+++ src/test/java/com/sb02/guestbookservice/GuestbookServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.sb02.guestbookservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class GuestbookServiceApplicationTests {
-
-  @Test
-  void contextLoads() {
-  }
-
-}
+//package com.sb02.guestbookservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GuestbookServiceApplicationTests {
+//
+//  @Test
+//  void contextLoads() {
+//  }
+//
+//}
